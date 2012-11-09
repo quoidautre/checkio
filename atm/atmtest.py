457ff1bf28c880f3ff2d390b503d79c4dbbb7396
Carl Bergquist <carl.bergquist@gmail.com>
2012-11-09 22:47:54 +0100

    withdraw now takes an array instead

--- atm/atmtest.py
+++ atm/atmtest.py
@@ -6,11 +6,11 @@ class TestWithdrawAmount(unittest.TestCase):
 		self.atm = atm.Atm(self.startValue)
 
 	def test_can_withdraw_five(self):
-		self.atm.withdraw(5)
+		self.atm.withdraw([ 5 ])
 		self.assertNotEqual(self.atm.amount, self.startValue)
 
 	def test_cannot_withdraw_seven(self):
-		self.atm.withdraw(7)
+		self.atm.withdraw([ 7 ])
 		self.assertEqual(self.atm.amount, self.startValue)
 
 class TestCommisionCalculator(unittest.TestCase):
@@ -25,10 +25,8 @@ class TestCommisionCalculator(unittest.TestCase):
 		self.assertEqual(1, self.cc.calc(50))
 
 class TestAtm(unittest.TestCase):
-	def test_only_withdraw_if_dividable_with_5(self):
-		a = atm.Atm(100)
-		a.withdraw(4)
-		self.assertEqual(a.amount, 100)
+	def setUp(self):
+		return 2
 
 	#def test_checkio(self):
 		#self.assertTrue(checkio([120, [10, 20, 30]]) == 57)
