9fcc66dd907bf2885e69f2126f941deb6559c789
Carl Bergquist <carl.bergquist@gmail.com>
2012-11-09 23:14:36 +0100

    removed testcode

--- atm/atmtest.py
+++ atm/atmtest.py
@@ -1,39 +1,31 @@
-import atm, unittest
+import math
 
-class TestWithdrawAmount(unittest.TestCase):
-	def setUp(self):
-		self.startValue = 100
-		self.atm = atm.Atm(self.startValue)
+def calculateCommision(asdf):
+	return 0.5 + (amountToWithdraw * 0.01)
 
-	def test_can_withdraw_five(self):
-		self.atm.withdraw([ 5 ])
-		self.assertNotEqual(self.atm.amount, self.startValue)
+def atm(balance, withdraws):
+	withdraw = withdraws.pop(0)
 
-	def test_cannot_withdraw_seven(self):
-		self.atm.withdraw([ 7 ])
-		self.assertEqual(self.atm.amount, self.startValue)
+	if withdraw % 5 not 0:
+		return atm(balance, withdraws)
 
-class TestCommisionCalculator(unittest.TestCase):
-	def setUp(self):
-		self.cc = atm.CommisionCalculator()
+	commision = math.ceil(0.5 +(withdraw * 0.01))
 
-	def test_always_return_base_commision(self):
-		self.assertEqual(0.5, self.cc.calc(0))
+	if commision <= balance:
+		balance += commision
 
-	def test_and_one_percentage(self):
-		self.assertEqual(1.5, self.cc.calc(100))
-		self.assertEqual(1, self.cc.calc(50))
+	return balance if len(withdraws) == 0 else atm(balance, withdraws)
 
-class TestAtm(unittest.TestCase):
-	def setUp(self):
-		return 2
+def checkio(input):
+	balance = input[0]
+	withdraws = input[1]
 
-	#def test_checkio(self):
-		#self.assertTrue(checkio([120, [10, 20, 30]]) == 57)
-		#self.assertTrue(checkio([120, [200, 10]]) == 109)
-		#self.assertTrue(checkio([120,[3, 10]]) == 109)
-		#self.assertTrue(checkio([120, [200 , 119]]) == 120)
-		#self.assertTrue(checkio([120, [120, 10, 122, 2, 10, 10, 30, 1]]) == 56)
+	return atm(balance, withdraws)
+
 
 if __name__ == '__main__':
-	unittest.main()
+	assert(checkio([120, [10, 20, 30]]) == 57)
+	assert(checkio([120, [200, 10]]) == 109)
+	assert(checkio([120, [3, 10]]) == 109)
+	assert(checkio([120, [200 , 119]]) == 120)
+	assert(checkio([120, [120, 10, 122, 2, 10, 10, 30, 1]]) == 56)
