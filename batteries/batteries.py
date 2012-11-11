130700859ea7fe0325eadad58c96bb2be480449c
Carl Bergquist <carl.bergquist@gmail.com>
2012-11-11 18:34:14 +0100

    added one more condition

--- batteries/batteries.py
+++ batteries/batteries.py
@@ -22,3 +22,4 @@ if __name__ == '__main__':
 	#assert checkio([5,5,6,5]) == 1
 	assert checkio([12, 30, 30, 32, 42, 49]) == 9
 	#assert checkio([1, 1, 1, 3]) == 0
+	#assert checkio([9, 9, 7, 6, 5]) == 0
