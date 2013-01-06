2427de278f5d27afba795feb4ead3ff65332bf75
Carl Bergquist <carl.bergquist@gmail.com>
2013-01-06 19:19:53 +0100

    Added invalid tests

--- spaceship/spaceship.py
+++ spaceship/spaceship.py
@@ -1,11 +1,24 @@
 def checkio(input):
-	sofia = range(input[0], input[2], input[1])
-	salesman = range(input[2], 0, (input[3] * -1))
+    sofia = range(input[0], input[2], input[1])
+    sofia.append(sofia[-1] + input[1])
+    salesman = range(input[2], 0, (input[3] * -1))
+    print salesman[-1]
 
-	for bid in zip(sofia, salesman):
-		if bid[0] >= bid[1]:
-			return bid[0]
+    if not len(sofia):
+        return input[2]
+
+    z = zip(sofia, salesman)
+
+    print sofia
+    print salesman
+    print z
+
+    for bid in z:
+        if bid[0] >= bid[1]:
+            return bid[0]
 
 if __name__ == '__main__':
-	assert checkio([150, 50, 1000, 100]) == 450
-	assert checkio([150, 50, 900, 100]) == 400
+    #assert checkio([150, 50, 1000, 100]) == 450
+    #assert checkio([150, 50, 900, 100]) == 400
+    #assert checkio([200, 100, 200, 100]) == 200
+    assert checkio([500, 300, 700, 50]) == 700
