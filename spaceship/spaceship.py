b3c84d18fb6b496baf08f813bc87ce08fc2e3042
Carl Bergquist <carl.bergquist@gmail.com>
2013-01-07 21:59:44 +0100

    Fixed spaceship task

--- spaceship/spaceship.py
+++ spaceship/spaceship.py
@@ -1,24 +1,23 @@
 def checkio(input):
-    sofia = range(input[0], input[2], input[1])
-    sofia.append(sofia[-1] + input[1])
-    salesman = range(input[2], 0, (input[3] * -1))
-    print salesman[-1]
+    sofiaBid = input[0]
+    sofiaInc = input[1]
+    salesmanOffer = input[2]
+    salesmanDec = input[3]
 
-    if not len(sofia):
-        return input[2]
+    while(True):
+        if sofiaBid >= salesmanOffer:
+            return sofiaBid
 
-    z = zip(sofia, salesman)
+        sofiaBid += sofiaInc
 
-    print sofia
-    print salesman
-    print z
+        if sofiaBid >= salesmanOffer:
+            return salesmanOffer
 
-    for bid in z:
-        if bid[0] >= bid[1]:
-            return bid[0]
+        salesmanOffer -= salesmanDec
+
 
 if __name__ == '__main__':
-    #assert checkio([150, 50, 1000, 100]) == 450
-    #assert checkio([150, 50, 900, 100]) == 400
-    #assert checkio([200, 100, 200, 100]) == 200
+    assert checkio([150, 50, 1000, 100]) == 450
+    assert checkio([150, 50, 900, 100]) == 400
+    assert checkio([200, 100, 200, 100]) == 200
     assert checkio([500, 300, 700, 50]) == 700
