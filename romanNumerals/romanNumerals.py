c6e29544543af3041c541c0abdc48d53b59e8f78
Carl Bergquist <carl.bergquist@gmail.com>
2013-01-12 21:42:48 +0100

    Solved roman numerals

--- romanNumerals/romanNumerals.py
+++ romanNumerals/romanNumerals.py
@@ -1,6 +1,33 @@
+romans = [
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'LX'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I')
+]
+
+
 def checkio(num):
-    pass
+    result = ''
+
+    for n, r in romans:
+        while n <= num:
+            result += r
+            num -= n
+
+    return result
+
 
 if __name__ == '__main__':
     assert checkio(6) == 'VI'
     assert checkio(76) == 'LXXVI'
+    assert checkio(499) == 'CDXCIX'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII'
--- romanNumerals/romanNumeralsTest.py
+++ romanNumerals/romanNumeralsTest.py
@@ -9,5 +9,17 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_76(self):
         self.assertEqual(romanNumerals.checkio(76), 'LXXVI')
 
+    def test_499(self):
+        self.assertEqual(romanNumerals.checkio(499), 'CDXCIX')
+
+    def test_3888(self):
+        self.assertEqual(romanNumerals.checkio(3888), 'MMMDCCCLXXXVIII')
+
+    def test_1999(self):
+        self.assertEqual(romanNumerals.checkio(1999), 'MCMXCIX')
+
+    def test_1900(self):
+        self.assertEqual(romanNumerals.checkio(1900), 'MCM')
+
 if __name__ == '__main__':
     unittest.main()
