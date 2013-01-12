ce3e52ff173791602b4ba344a56f384e287c0d7d
Carl Bergquist <carl.bergquist@gmail.com>
2013-01-12 14:31:39 +0100

    Added tests for polish notation

--- polishNotation/polish-notation-test.py
+++ polishNotation/polish-notation-test.py
@@ -1,8 +1,22 @@
 import unittest
+import polishNotation
 
 
 class PolishNotationTest(unittest.TestCase):
-    pass
+    def test_0(self):
+        self.assertEqual(polishNotation.checkio("a*b"), "ab*")
+
+    def test_1(self):
+        self.assertEqual(polishNotation.checkio("(a+(b*c))"), "abc*+")
+
+    def test_2(self):
+        self.assertEqual(polishNotation.checkio("((a+b)*(z+x))"), "ab+zx+*")
+
+    def test_3(self):
+        self.assertEqual(polishNotation.checkio("((a+t)*((b+(a+c))^(c+d)))"), "at+bac++cd+^*")
+
+    def test_4(self):
+        self.assertEqual(polishNotation.checkio("a+b*c+d"), "abc*+d+")
 
 
 if __name__ == '__main__':
