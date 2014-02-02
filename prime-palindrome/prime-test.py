2359b1189122286e7b97168d9687f3712c1e7b0e
Carl Bergquist <carl.bergquist@gmail.com>
2014-02-02 14:02:20 +0100

    solved prime palindrome

--- prime-palindrome/prime-test.py
+++ prime-palindrome/prime-test.py
@@ -7,10 +7,13 @@ class PrimeTest(unittest.TestCase):
         self.assertEqual(prime.checkio(31), 101)
 
     def test_2(self):
-        self.assertEqual(checkio(130), 131)
+        self.assertEqual(prime.checkio(130), 131)
 
     def test_3(self):
-        self.assertEqual(checkio(131), 131)
+        self.assertEqual(prime.checkio(131), 131)
+
+    def test_4(self):
+        self.assertTrue(prime.is_palindrome('101'))
 
 if __name__ == '__main__':
     unittest.main()
--- prime-palindrome/prime.py
+++ prime-palindrome/prime.py
@@ -1,7 +1,33 @@
-def checkio(input):
-	pass
+def is_palindrome(input):
+    s = str(input)
+    le = len(s)
+    for x in xrange((le / 2) + 1):
+        if s[x] != s[le - x -1]:
+            return False
+
+    return True
+
+def gen_primes():
+    D = {}
+    q = 2
+
+    while True:
+        if q not in D:
+            yield q
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+
+def checkio(lowest):
+    for prime in gen_primes():
+        if prime >= lowest and is_palindrome(prime):
+            return prime
 
 if __name__ == '__main__':
-	assert(checkio(31) == 101)
-	assert(checkio(130) == 131)
-	assert(checkio(131) == 131)
+    assert(checkio(31) == 101)
+    assert(checkio(130) == 131)
+    assert(checkio(131) == 131)
